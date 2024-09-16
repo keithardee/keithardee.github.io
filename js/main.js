894ddf86b49b3aff6f8e4593f0ba36b1bbf902d1
Keith Ardee <150048707+keithardee@users.noreply.github.com>
2024-09-16 21:06:13 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* class is from html in text inside home-content */
+var typed = new Typed(".text", {
+   strings:["Frontend Developer", "2nd Year College", "Web Developer"],
+   typeSpeed:100,
+   backSpeed:100,
+   backDelay:1000,
+   loop:true
+})
