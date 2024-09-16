ffceaf5b90b80f061e58a8f6b4edf2d7e3e67bac
Keith Ardee <150048707+keithardee@users.noreply.github.com>
2024-09-16 21:18:01 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-/* class is from html in text inside home-content */
-var typed = new Typed(".text", {
-   strings:["Frontend Developer", "2nd Year College", "Web Developer"],
-   typeSpeed:100,
-   backSpeed:100,
-   backDelay:1000,
-   loop:true
-})
